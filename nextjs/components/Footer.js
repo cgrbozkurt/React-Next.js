c575ba62f9424aa58313bbbc4e349eb6d458120d
cgrbozkurt <cagribozzkurt@gmail.com>
2023-11-23 01:26:01 +0300

    nextjs

---
+++ nextjs/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-red-300'>Footer asdf</div>
+  )
+}
+
+export default Footer
---
+++ nextjs/pages/product.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Product = () => {
+  return (
+<div>
+    <Head>
+        <title>Product Pages</title>
+    </Head>
+<div>Product</div>
+
+</div>
+    )
+}
+
+export default Product
